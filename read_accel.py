8aa84fdd2a93006d2b4bc9c361fd6ab16db60e4f
Rosenbez <zacharyrrosenberg@gmail.com>
2019-02-17 21:10:00 -0500

    inital code to read accel data over network

---
+++ read_accel.py
@@ -0,0 +1,43 @@
+import socket
+import datetime
+import yaml
+import csv
+
+
+def write_data(data_dict):
+    with open('accel_logs.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for t in data_dict:
+            writer.writerow([str(t), data_dict[t]['x'],
+                            data_dict[t]['y'],
+                            data_dict[t]['z']])
+    print('data written')
+
+
+UDP_IP= '192.168.7.54'
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+print('Starting UPD Socket')
+accel_buf ={}
+buf_lim = 10
+buf_count = 0
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    ts = datetime.datetime.now()
+    accel_data = yaml.load(data)
+    print('recieved message:', data.decode('utf-8'))
+    print(datetime.datetime.now())
+    print('from:', addr)
+    
+    accel_buf[ts] = accel_data
+    buf_count += 1
+    
+    if buf_count >= buf_lim:
+        write_data(accel_buf)
+        accel_buf = {}
+        buf_count = 0
+
